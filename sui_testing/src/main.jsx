c21aab50634b7a38480c5a1e40ba3f1c77678cf5
AkashMundari <akashmundari71@gmail.com>
2025-07-29 00:59:07 +0530

    create src escrow done with frontend

---
+++ sui_testing/src/main.jsx
@@ -0,0 +1,30 @@
+// src/index.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "@mysten/dapp-kit/dist/index.css";
+import "./index.css";
+
+import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Set up network config
+const networks = {
+  devnet: { url: getFullnodeUrl("devnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+};
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networks} defaultNetwork="devnet">
+        <WalletProvider autoConnect>
+          <App />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
