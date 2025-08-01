c220aa08252281e73ce405599000aad22b7d7f6d
samarth <samarth.jindal2004@gmail.com>
2025-08-01 12:17:54 +0530

    removed old unused folders

--- server/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import dotenv from "dotenv";
-
-dotenv.config();
-export const supabase = createClient(process.env.SB_PROJECT_URL, process.env.SB_SERVICE_KEY);
-console.log("Supabase client connected");
